321dca2d0e544a783bd03f75fa9e72a82152aad4
j-j-martin <jermaine.martin@hamburger-software.de>
2024-03-11 18:59:13 +0100

    pipeline test 7 (no validate request)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import { validateRequest } from '@/lib/auth'
 import { redirect } from 'next/navigation'
 
 export default async function Page() {
-  const { user } = await validateRequest()
+  //const { user } = await validateRequest()
   // if (user) {
   //   return redirect('/')
   // }
