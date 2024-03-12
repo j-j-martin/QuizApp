ebae544086f5d7d1996c3b6d6edfe6c6889f1e13
j-j-martin <jermaine.martin@hamburger-software.de>
2024-03-12 10:20:57 +0100

    Pipeline test 17: Check if github variables are accessible

--- src/app/login/github/route.ts
+++ src/app/login/github/route.ts
@@ -1,13 +1,20 @@
 import { generateState } from 'arctic'
 import { github } from '@/lib/auth'
 import { cookies } from 'next/headers'
+import crypto from 'crypto'
 
 import { NextResponse } from 'next/server'
 
 export async function GET(): Promise<NextResponse> {
   const state = generateState()
   //const url = await github.createAuthorizationURL(state)
-  console.log('Hallo')
+  if (process.env.GITHUB_CLIENT_ID && process.env.GITHUB_CLIENT_SECRET) {
+    // Environment variables are accessible
+    console.log('GitHub client ID and secret are accessible.')
+  } else {
+    // Environment variables are not accessible
+    console.error('GitHub client ID and/or secret are not accessible.')
+  }
 
   cookies().set('github_oauth_state', state, {
     path: '/',
