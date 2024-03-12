0f2ba67c9804f764071691b0a87df9ed44c53ef5
j-j-martin <jermaine.martin@hamburger-software.de>
2024-03-12 11:00:40 +0100

    Pipeline test 18: Run prisma generate after install

--- src/app/login/github/route.ts
+++ src/app/login/github/route.ts
@@ -1,13 +1,12 @@
 import { generateState } from 'arctic'
 import { github } from '@/lib/auth'
 import { cookies } from 'next/headers'
-import crypto from 'crypto'
 
 import { NextResponse } from 'next/server'
 
 export async function GET(): Promise<NextResponse> {
   const state = generateState()
-  //const url = await github.createAuthorizationURL(state)
+  const url = await github.createAuthorizationURL(state)
   if (process.env.GITHUB_CLIENT_ID && process.env.GITHUB_CLIENT_SECRET) {
     // Environment variables are accessible
     console.log('GitHub client ID and secret are accessible.')
