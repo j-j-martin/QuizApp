6170cf8f4786c5925c6be82b0ccf125fd7a63165
Noah Forchner <noah.forchner@hamburger-software.de>
2024-03-15 10:08:05 +0100

    welcome message

--- src/app/UserPage/page.tsx
+++ src/app/UserPage/page.tsx
@@ -9,10 +9,16 @@ const UserPage = () => {
       <BackgroundBeams />
       <Navbar />
       <div className='flex flex-col w-full h-[100vh] content-center relative z-2'>
-        <p className='mx-auto my-auto text-xl font-semibold transition ease-in-out delay-150 hover:scale-105'>Wilkommen -USERNAME-</p>
+        
+      <div className="max-w-2xl mx-auto p-4">
+        <h1 className="relative z-10 text-lg md:text-6xl bg-clip-text text-transparent bg-gradient-to-b from-yellow-200 to-yellow-600  text-center font-sans font-bold">
+          Wilkommen Username
+        </h1>
+      </div>
         <Button className='mx-auto my-auto h-16 w-48 text-xl transition ease-in-out delay-150 hover:scale-105'>Spiel Starten</Button>
       </div>
     </div>
+    
   )
 }
 
--- src/components/login-mask.tsx
+++ src/components/login-mask.tsx
@@ -1,4 +1,4 @@
-import { Label } from '@radix-ui/react-label'
+import { Label } from './ui/label'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card'
 import { Button } from './ui/button'
 import { Input } from './ui/input'
