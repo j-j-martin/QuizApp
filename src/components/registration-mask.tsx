92e04c4d948d995cf8b6f3a987e9f1104a5e27f1
j-j-martin <jermaine.martin@hamburger-software.de>
2024-04-02 11:30:06 +0200

    Login groß

--- src/components/registration-mask.tsx
+++ src/components/registration-mask.tsx
@@ -6,56 +6,53 @@ import { Input } from './ui/input'
 import { Label } from './ui/label'
 import { Icons } from './ui/icons'
 import { useRouter } from 'next/navigation'
-import { BackgroundGradient } from './ui/background-gradient'
 
 export default function RegistrationMask() {
   const router = useRouter()
   return (
     <div>
-      <BackgroundGradient className='rounded-[22px] maw-w-md mx-auto my-auto p-4 sm:p-10 bg-white dark:bg-zinc-900'>
-        <Card className='z-10 relative max-w-md max-h-lg mx-auto my-auto mt-56 border-style-none'>
-          <CardHeader className='space-y-1'>
-            <CardTitle className='text-2xl'>Create an account</CardTitle>
-            <CardDescription>Enter your email below to create your account</CardDescription>
-          </CardHeader>
-          <CardContent className='grid gap-4'>
-            <div className='grid grid-cols-2 gap-6'>
-              <Button variant='outline'>
-                <Icons.gitHub className='mr-5 h-6 w-5' />
-                Github
-              </Button>
-              <Button variant='outline'>
-                <Icons.google className='mr-2 h-5 w-5' />
-                Google
-              </Button>
+      <Card className='z-10 relative max-w-md max-h-lg mx-auto my-auto mt-56 border-style-none'>
+        <CardHeader className='space-y-1'>
+          <CardTitle className='text-2xl'>Create an account</CardTitle>
+          <CardDescription>Enter your email below to create your account</CardDescription>
+        </CardHeader>
+        <CardContent className='grid gap-4'>
+          <div className='grid grid-cols-2 gap-6'>
+            <Button variant='outline'>
+              <Icons.gitHub className='mr-5 h-6 w-5' />
+              Github
+            </Button>
+            <Button variant='outline'>
+              <Icons.google className='mr-2 h-5 w-5' />
+              Google
+            </Button>
+          </div>
+          <div className='relative'>
+            <div className='absolute inset-0 flex items-center'>
+              <span className='w-full border-t' />
             </div>
-            <div className='relative'>
-              <div className='absolute inset-0 flex items-center'>
-                <span className='w-full border-t' />
-              </div>
-              <div className='relative flex justify-center text-xs uppercase'>
-                <span className='bg-background px-2 text-muted-foreground'>Or continue with</span>
-              </div>
+            <div className='relative flex justify-center text-xs uppercase'>
+              <span className='bg-background px-2 text-muted-foreground'>Or continue with</span>
             </div>
-            <div className='grid gap-2'>
-              <Label htmlFor='email'>Email</Label>
-              <Input id='email' type='email' placeholder='m@example.com' />
-            </div>
-            <div className='grid gap-2'>
-              <Label htmlFor='password'>Password</Label>
-              <Input id='password' type='password' />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <div className='w-full'>
-              <Button className='w-full mb-4'>Create account</Button>
-              <Button variant='outline' className='w-full' onClick={() => router.push('/')}>
-                Back
-              </Button>
-            </div>
-          </CardFooter>
-        </Card>
-      </BackgroundGradient>
+          </div>
+          <div className='grid gap-2'>
+            <Label htmlFor='email'>Email</Label>
+            <Input id='email' type='email' placeholder='m@example.com' />
+          </div>
+          <div className='grid gap-2'>
+            <Label htmlFor='password'>Password</Label>
+            <Input id='password' type='password' />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <div className='w-full'>
+            <Button className='w-full mb-4'>Create account</Button>
+            <Button variant='outline' className='w-full' onClick={() => router.push('/')}>
+              Back
+            </Button>
+          </div>
+        </CardFooter>
+      </Card>
     </div>
   )
 }
