7cdc409cbfdf017e3e32b5af4a7320fc3d1e07af
j-j-martin <jermaine.martin@hamburger-software.de>
2024-04-02 12:35:46 +0200

    Conditional login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,17 @@
+import Loginmask from '@/components/login-mask'
+import Navbar from '@/components/navbar'
 import { validateRequest } from '@/lib/auth'
 import { redirect } from 'next/navigation'
 
 export default async function Page() {
-  const { user } = await validateRequest()
-  if (user) {
-    return redirect('/')
-  }
+  // const { user } = await validateRequest()
+  // if (user) {
+  //   return redirect('/')
+  // }
   return (
-    <>
-      <h1>Sign in</h1>
-      <a href='/login/github'>Sign in with GitHub</a>
-    </>
+    <div className='antialiased'>
+      <Navbar />
+      <Loginmask />
+    </div>
   )
 }
