8a7e6b73f3eca53f1367c11885257a91b9757f98
j-j-martin <jermaine.martin@outlook.de>
2024-06-15 06:27:22 +0200

    pseudochange

---
+++ src/app/api/Questions/[id]/route.ts
@@ -0,0 +1,15 @@
+import { Prisma, PrismaClient } from '@prisma/client'
+import { NextRequest, NextResponse } from 'next/server'
+
+const prisma = new PrismaClient()
+
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+  try {
+    const questions = await prisma.question.findUnique({
+      where: { id: params.id },
+    })
+    return NextResponse.json(questions)
+  } catch (error) {
+    return new Response('Invalid request bodies', { status: 422 })
+  }
+}
