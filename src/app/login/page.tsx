dd2d2d797117ddd289db1c3a50922c815256e720
j-j-martin <jermaine.martin@outlook.de>
2024-06-15 09:41:02 +0200

    finalize

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,17 +5,24 @@ import Navbar from '@/components/navbar'
 import { validateRequest } from '@/lib/auth'
 import { redirect } from 'next/navigation'
 import { useEffect, useState } from 'react'
+import styles from './loginpage.module.css'
 
 export default function Page() {
   // const { user } = await validateRequest()
   // if (user) {
   //   return redirect('/')
   // }
-  const target = new Date('2024-12-31T23:59:59').getTime()
+  const target = new Date('2024-07-01T00:00:00').getTime()
   const calculateTimeDifference = () => {
     const currentTime = new Date().getTime()
     const difference = target - currentTime
-    return Math.floor((difference % (1000 * 60)) / 1000)
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24))
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000)
+
+    return { days, hours, minutes, seconds }
   }
 
   const [counter, setCounter] = useState(calculateTimeDifference())
@@ -31,7 +38,24 @@ export default function Page() {
   return (
     <div className='antialiased'>
       <Navbar />
-      Sekunden bis Shirleys 30en: {counter}
+      <div className={styles.countdownDiv}>
+        <h1 className={styles.countdownHeading}>Zeit bis Shirleys 30sten:</h1>
+        <div className={styles.splitContainer}>
+          <div className={styles.numberColumn}>
+            <p className={styles.countdownParagraph}>{counter.days}</p>
+            <p className={styles.countdownParagraph}>{counter.hours}</p>
+            <p className={styles.countdownParagraph}>{counter.minutes}</p>
+            <p className={styles.countdownParagraph}>{counter.seconds}</p>
+          </div>
+          <div className={styles.textColumn}>
+            <p className={styles.countdownParagraph}>Tage</p>
+            <p className={styles.countdownParagraph}>Stunden</p>
+            <p className={styles.countdownParagraph}>Minuten</p>
+            <p className={styles.countdownParagraph}>Sekunden</p>
+          </div>
+        </div>
+        <img alt='hello' src={'/troll-inverted.png'} width={500} height={500} />
+      </div>
       {/* <Loginmask /> */}
     </div>
   )
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,10 +4,8 @@ import { ModeToggle } from './mode-toggle'
 function Navbar() {
   return (
     <nav className='font-bold text-xl py-4 px-4 grid grid-cols-2'>
-      <div>BrainBuster</div>
-      <div className='justify-self-end'>
-        <ModeToggle />
-      </div>
+      <div></div>
+      <div className='justify-self-end'>{/* <ModeToggle /> */}</div>
     </nav>
   )
 }
